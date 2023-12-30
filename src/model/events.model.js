13fd3ad6bc9e46664b525513010747b0334335fe
Muhammad A SI F <muhammadasif.wd@gmail.com>
2023-12-31 00:32:15 +0600

    [MODIFY] some changes

--- src/model/events.model.js
+++ src/model/events.model.js
@@ -1,30 +1,36 @@
-const { mongoose } = require("mongoose");
+const { mongoose } = require('mongoose');
 const { Schema } = mongoose;
 
-const eventsSchema = new Schema({
-    title: {
-        type: String,
-        required: [true, 'Title is required'],
-        min: [10, 'Minimum 10 characters are required'],
-        max: [80, 'Maximum 80 characters are required']
-    },
-    date: {
-        type: String,
-        required: [true, 'Date is required']
-    },
-    location: {
-        type: String,
-        required: [true, 'Location is required'],
-    },
-    description: {
-        type: String,
-        required: [true, 'Description is required'],
-    }
-}, {
-    timestamps: true,
-    collection: "events",
-}
-)
+const eventsSchema = new Schema(
+	{
+		title: {
+			type: String,
+			required: [true, 'Title is required'],
+			min: [10, 'Minimum 10 characters are required'],
+			max: [80, 'Maximum 80 characters are required'],
+		},
+		img: {
+			type: String,
+			required: [true, 'Events image is required'],
+		},
+		date: {
+			type: String,
+			required: [true, 'Date is required'],
+		},
+		location: {
+			type: String,
+			required: [true, 'Location is required'],
+		},
+		description: {
+			type: String,
+			required: [true, 'Description is required'],
+		},
+	},
+	{
+		timestamps: true,
+		collection: 'events',
+	}
+);
 
-const Events = mongoose.model("Events", eventsSchema);
+const Events = mongoose.model('Events', eventsSchema);
 module.exports = Events;
--- src/validation/events.validation.js
+++ src/validation/events.validation.js
@@ -1,80 +1,66 @@
 const Joi = require('joi');
 
 const createEventsValidationSchema = Joi.object({
-    title: Joi.string()
-        .min(10)
-        .max(80)
-        .required()
-        .messages({
-            'string.base': 'Event should be a string',
-            'string.empty': 'Event title is required',
-            'string.min': 'Minimum 10 characters are required for the name',
-            'string.max': 'Maximum 80 characters are allowed for the name',
-            'any.required': 'Event Title is required',
-        }),
-    date: Joi.string()
-        .required()
-        .messages({
-            'string.base': 'Event date should be a string',
-            'string.empty': 'Event date is required',
-            'any.required': 'Event date is required',
-        }),
-    location: Joi.string()
-        .required()
-        .messages({
-            'string.base': 'Location should be a string',
-            'string.empty': 'Event Location is required',
-            'any.required': 'Event Location is required',
-        }),
-    description: Joi.string()
-        .required()
-        .messages({
-            'string.base': 'Event description should be a string',
-            'string.empty': 'Event description is required',
-            'any.required': 'Event description is required',
-        }),
+	title: Joi.string().min(10).max(80).required().messages({
+		'string.base': 'Event should be a string',
+		'string.empty': 'Event title is required',
+		'string.min': 'Minimum 10 characters are required for the name',
+		'string.max': 'Maximum 80 characters are allowed for the name',
+		'any.required': 'Event Title is required',
+	}),
+	img: Joi.string().required().messages({
+		'string.base': 'Event image should be a url',
+		'string.empty': 'Event image is required',
+		'any.required': 'Event image is required',
+	}),
+	date: Joi.string().required().messages({
+		'string.base': 'Event date should be a string',
+		'string.empty': 'Event date is required',
+		'any.required': 'Event date is required',
+	}),
+	location: Joi.string().required().messages({
+		'string.base': 'Location should be a string',
+		'string.empty': 'Event Location is required',
+		'any.required': 'Event Location is required',
+	}),
+	description: Joi.string().required().messages({
+		'string.base': 'Event description should be a string',
+		'string.empty': 'Event description is required',
+		'any.required': 'Event description is required',
+	}),
 });
 
 const updateEventsValidationSchema = Joi.object({
-    title: Joi.string()
-        .min(10)
-        .max(80)
-        .required()
-        .optional()
-        .messages({
-            'string.base': 'Event should be a string',
-            'string.empty': 'Event title is required',
-            'string.min': 'Minimum 10 characters are required for the name',
-            'string.max': 'Maximum 80 characters are allowed for the name',
-            'any.required': 'Event Title is required',
-        }),
-    date: Joi.string()
-        .required()
-        .optional()
-        .messages({
-            'string.base': 'Event date should be a string',
-            'string.empty': 'Event date is required',
-            'any.required': 'Event date is required',
-        }),
-    location: Joi.string()
-        .required()
-        .optional()
-        .messages({
-            'string.base': 'Location should be a string',
-            'string.empty': 'Event Location is required',
-            'any.required': 'Event Location is required',
-        }),
-    description: Joi.string()
-        .required()
-        .optional()
-        .messages({
-            'string.base': 'Event description should be a string',
-            'string.empty': 'Event description is required',
-            'any.required': 'Event description is required',
-        }),
+	title: Joi.string().min(10).max(80).required().optional().messages({
+		'string.base': 'Event should be a string',
+		'string.empty': 'Event title is required',
+		'string.min': 'Minimum 10 characters are required for the name',
+		'string.max': 'Maximum 80 characters are allowed for the name',
+		'any.required': 'Event Title is required',
+	}),
+	img: Joi.string().required().optional().messages({
+		'string.base': 'Event image should be a url',
+		'string.empty': 'Event image is required',
+		'any.required': 'Event image is required',
+	}),
+	date: Joi.string().required().optional().messages({
+		'string.base': 'Event date should be a string',
+		'string.empty': 'Event date is required',
+		'any.required': 'Event date is required',
+	}),
+	location: Joi.string().required().optional().messages({
+		'string.base': 'Location should be a string',
+		'string.empty': 'Event Location is required',
+		'any.required': 'Event Location is required',
+	}),
+	description: Joi.string().required().optional().messages({
+		'string.base': 'Event description should be a string',
+		'string.empty': 'Event description is required',
+		'any.required': 'Event description is required',
+	}),
 });
 
 module.exports = {
-    createEventsValidationSchema,
-    updateEventsValidationSchema,
+	createEventsValidationSchema,
+	updateEventsValidationSchema,
 };
